0aa5614f2a7732fb94e3f7fb98b35ec23e6e02ca
YasinAkdeniz <yasnakdeniz@gmail.com>
2024-08-30 18:11:36 +0300

    Promotion uygulandı ve total fiyat hesaplama yapıldı

--- src/main/java/com/example/springProject/Controller/CartController.java
+++ src/main/java/com/example/springProject/Controller/CartController.java
@@ -2,8 +2,10 @@ package com.example.springProject.Controller;
 
 import com.example.springProject.Model.Cart;
 import com.example.springProject.Model.Products;
+import com.example.springProject.Model.Promotion;
 import com.example.springProject.Model.User;
 import com.example.springProject.Repository.ProductRepo;
+import com.example.springProject.Repository.PromotionRepo;
 import com.example.springProject.Repository.UserRepo;
 import com.example.springProject.Service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +26,15 @@ public class CartController {
     @Autowired
     ProductRepo productRepo;
 
+    @Autowired
+    PromotionRepo promotionRepo;
+
     @PostMapping("/addCart")
-    public ResponseEntity<Cart> addCart(@RequestParam Long userId, @RequestParam Long productId) {
+    public ResponseEntity<Cart> addCart(@RequestParam Long userId, @RequestParam Long productId, @RequestParam Long promotionId) {
         User user = userRepo.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
         Products products = productRepo.findById(productId).orElseThrow(()-> new RuntimeException("Product Not Fount"));
-        Cart cart = cartService.addCart(user,products);
+        Promotion promotion = promotionRepo.findById(promotionId).orElseThrow(()-> new RuntimeException("Promotion Not Fount"));
+        Cart cart = cartService.addCart(user,products,promotion);
 
         return ResponseEntity.ok(cart);
     }
--- src/main/java/com/example/springProject/Service/CartService.java
+++ src/main/java/com/example/springProject/Service/CartService.java
@@ -1,7 +1,9 @@
 package com.example.springProject.Service;
 
+import com.example.springProject.Enum.PromotionEnum;
 import com.example.springProject.Model.Cart;
 import com.example.springProject.Model.Products;
+import com.example.springProject.Model.Promotion;
 import com.example.springProject.Model.User;
 import com.example.springProject.Repository.CartRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +15,26 @@ public class CartService {
     @Autowired
     CartRepo cartRepo;
 
-    public Cart addCart(User user, Products products) {
+
+    public Cart addCart(User user, Products products, Promotion promotion) {
         Cart cart = new  Cart();
         cart.setUser(user);
         cart.setProducts(products);
-        cart.setTotalPrice(products.getPrice());
+        cart.setPromotion(promotion);
+        cart.setTotalPrice(calculateTotalPrice(promotion, products.getPrice()));
 
         return cartRepo.save(cart);
+    }
+
+    public Long calculateTotalPrice(Promotion promotion, Long totalPrice)  {
+       if (promotion.getType() == PromotionEnum.RATE) {
+           totalPrice = (totalPrice / 100) * promotion.getDiscount();
+       } else if (promotion.getType() == PromotionEnum.PRICE) {
+           totalPrice = totalPrice - promotion.getDiscount();
+       }
+        System.out.println(totalPrice);
+
+        return  totalPrice;
+
     }
 }
