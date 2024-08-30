91529bff69c8e1742a868afb60a25b02482855f6
YasinAkdeniz <yasnakdeniz@gmail.com>
2024-08-30 17:21:03 +0300

    Promotion crud işlemleri

--- src/main/java/com/example/springProject/Controller/PromotionController.java
+++ src/main/java/com/example/springProject/Controller/PromotionController.java
@@ -1,4 +1,41 @@
 package com.example.springProject.Controller;
 
+import com.example.springProject.Model.Promotion;
+import com.example.springProject.Service.PromotionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class PromotionController {
+
+    @Autowired
+    private PromotionService promotionService;
+
+    @GetMapping("/promotions")
+    public List<Promotion> getAllPromotion(){
+       return promotionService.getPromotions();
+    }
+
+    @PostMapping("/promotion/add")
+    public Promotion addPromotion(@RequestBody Promotion promotion) {
+        return promotionService.addPromotion(promotion);
+    }
+
+    @GetMapping("/promotion/{id}")
+    @ResponseBody
+    public Promotion getById(@PathVariable("id") Long id) {
+        return promotionService.getPromotionById(id);
+    }
+
+    @PutMapping("/promotion/{id}")
+    public Promotion updatePromotion(@PathVariable("id") Long id, @RequestBody Promotion Promotion) {
+        return promotionService.updatePromotion(id, Promotion);
+
+    }
+    @DeleteMapping("/promotion/{id}")
+    public Promotion deletePromotion(@PathVariable("id") Long id){
+        return promotionService.deletePromotion(id);
+    }
 }
---
+++ src/main/java/com/example/springProject/Service/PromotionService.java
@@ -0,0 +1,51 @@
+package com.example.springProject.Service;
+
+import com.example.springProject.Model.Promotion;
+import com.example.springProject.Repository.PromotionRepo;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PromotionService {
+
+    @Autowired
+    PromotionRepo promotionRepo;
+
+    public List<Promotion> getPromotions(){
+        return promotionRepo.findAll();
+    }
+
+    public Promotion addPromotion(Promotion promotion) {
+        Promotion promotion1 = new Promotion();
+        promotion1.setId(promotion.getId());
+        promotion1.setName(promotion.getName());
+        promotion1.setType(promotion.getType());
+
+        return promotionRepo.save(promotion1);
+    }
+
+    public Promotion getPromotionById(Long id) {
+        return promotionRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("Promotion not found"));
+    }
+
+    public Promotion updatePromotion(Long id, Promotion promotion){
+        Promotion promotion1 = promotionRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("Product not found"));
+        promotion1.setId(promotion.getId());
+        promotion1.setName(promotion.getName());
+        promotion1.setName(promotion.getName());
+
+        return promotionRepo.save(promotion1);
+    }
+
+    public Promotion deletePromotion(Long id) {
+        Promotion promotion =  promotionRepo.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("There is no promotion id : " + id)
+        );
+        promotionRepo.delete(promotion);
+        return promotion;
+    }
+}
